0a72dbb0e395f0b9ea3ded74d841d19a4a1303dd
Brendan Ha <ha.brendan07@gmail.com>
2018-06-18 20:58:03 -0700

    added handlebars, controller, views, and routes

---
+++ controller/controllers.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+
+// Routes
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.get('/locations',  (req, res) => {
+  res.render('locations');
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact');
+})
+
+router.get('/menu', (req, res) => {
+  res.render('menu');
+});
+
+router.get('/jobs', (req, res) => {
+  res.render('join');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var bodyParser = require("body-parser")
 var methodOverride = require("method-override");
+var nodemailer = require("nodemailer");
 var path = require("path");
+var exphbs = require("express-handlebars")
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3030;
+
+// View engine setup
+app.set('views', path.join(__dirname, "views"));
+app.engine('handlebars', exphbs({ defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+var routes = require('./controller/controllers.js')
+app.use('/', routes);
 
-app.listen(port);
-console.log("App is listening on port: " + port);
+
+app.listen(port, () => console.log("App is listening on port: " + port));
